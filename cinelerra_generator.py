8c28db7a975fa942dfd7b4a61da4f8875be74f05
chaos-soft <mail@57st.su>
2024-11-19 20:12:49 +0300

    v20241119a

--- cinelerra_generator.py
+++ cinelerra_generator.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
-from typing import Any
+from pathlib import Path
+from typing import Any, Self
 import argparse
 import sys
 
 import yaml
 
+FADE_DELTA: int = 15
 FADE_MIN: int = -80
 
+type_keyframe = dict[int, str]
 type_strip = dict[str, Any]
 
 strips: list[type_strip] = []
@@ -20,75 +23,97 @@ class Channel():
         self.sequences = []
 
     def get_left_sequence(self, strip):
-        return get_left_sequence(strip, self.sequences)
+        left = None
+        for v in self.sequences:
+            if v == strip:
+                return left
+            left = v
+        return None
 
 
 class Strip():
-    type = 'AUDIO'
-    xml = '<EDIT STARTSOURCE={} CHANNEL={} LENGTH={} HARD_LEFT=0 HARD_RIGHT=0 COLOR=0 GROUP_ID=0><FILE SRC="{}"></FILE></EDIT>'  # noqa: E501
-    xml_empty = '<EDIT STARTSOURCE=0 CHANNEL=0 LENGTH={} HARD_LEFT=0 HARD_RIGHT=0 COLOR=0 GROUP_ID=0></EDIT>'
-    xml_fade = '<AUTO POSITION={} VALUE={} VALUE1=0 CONTROL_IN_VALUE=0 CONTROL_OUT_VALUE=0 TANGENT_MODE=0></AUTO>'
+    channel: int = 3
+    frame_final_end: int = 0
+    type: str = 'AUDIO'
+    xml: str = '<EDIT STARTSOURCE={} CHANNEL={} LENGTH={} HARD_LEFT=0 HARD_RIGHT=0 COLOR=0 GROUP_ID=0 USER_TITLE="{}"><FILE SRC="{}"></FILE></EDIT>'  # noqa: E501
+    xml_empty: str = '<EDIT STARTSOURCE=0 CHANNEL=0 LENGTH={} HARD_LEFT=0 HARD_RIGHT=0 COLOR=0 GROUP_ID=0></EDIT>'
+    xml_fade: str = '<AUTO POSITION={} VALUE={} VALUE1=0 CONTROL_IN_VALUE=0 CONTROL_OUT_VALUE=0 TANGENT_MODE=0></AUTO>'
 
-    def __init__(self, strip):
+    def __init__(self, strip, parent):
         self.audio_channels = [1, 0]
-        self.fade_in, self.fade_out = tuple(strip.get('fades', [0, 0]))
+        self.fade_in, self.fade_out, self.fade_in_position, self.fade_out_position = tuple(strip['fades'])
         self.filepath = strip['filepath']
-        self.get_channel(strip)
+        self.flags = strip.get('flags', [])
+        self.get_channel(strip, parent)
         self.get_offset_duration_position(strip)
         self.mutes = strip.get('mutes', [])
         self.volume = volume_levels.get(strip['filepath'], 0)
-        self.add_start_end()
+        self.volume_levels = strip.get('volume_levels', [])
+        self.add_start_end(parent)
 
-    def add_start_end(self):
-        last = get_last_sequence()
-        if last:
-            self.frame_final_end = last.frame_final_end + self.position + self.duration
-        else:
-            self.frame_final_end = self.position + self.duration
+    def __str__(self):
+        return '{} | {}'.format(Path(self.filepath).name, self.duration)
+
+    def add_start_end(self, parent):
+        self.frame_final_end = parent.frame_final_end + self.position + self.duration
         self.frame_final_start = self.frame_final_end - self.duration
 
     @property
     def duration(self):
-        if self.type == 'VIDEO' and self.channel == 3:
+        if self.is_video() and self.channel == 3:
             return self.duration_ - self.fade_out
         else:
             return self.duration_
 
     def extend_empty(self, left_fade_out, duration):
-        if self.type == 'VIDEO' and self.channel == 3:
+        if self.is_video() and self.channel == 3:
             duration += left_fade_out
         return duration
 
     def get_audio_channel(self):
-        if self.type == 'AUDIO':
+        if self.is_audio():
             return self.audio_channels.pop()
         return 0
 
-    def get_channel(self, strip: type_strip) -> None:
+    def get_channel(self, strip: type_strip, parent: Self) -> None:
         # Для Cinelerra делим на 2.
         self.channel = strip.get('channel', 0) // 2
         if not self.channel:
-            last = get_last_sequence()
-            if not last:
-                self.channel = 2
-                return None
             channels = [2, 3]
-            if last.channel in channels:
-                channels.remove(last.channel)
+            if parent.channel in channels:
+                channels.remove(parent.channel)
                 self.channel = channels[0]
             else:
                 self.channel = 2
 
     def get_delta(self):
-        return 800 if self.type == 'AUDIO' else 1
+        return 800 if self.is_audio() else 1
 
-    def get_fades_mutes(self):
-        fades = {}
+    def get_fades(self) -> type_keyframe:
+        keyframes: type_keyframe = {}
 
-        fades |= self.get_volume_level(self.frame_final_start, FADE_MIN)
-        fades |= self.get_volume_level(self.frame_final_start + self.fade_in // 4, self.volume - 10)
-        fades |= self.get_volume_level(self.frame_final_start + self.fade_in, self.volume)
+        if self.fade_in_position:
+            keyframes |= self.get_volume_level(self.frame_final_start, FADE_MIN)
+            position = self.frame_final_start + self.fade_in_position
+        else:
+            position = self.frame_final_start
+        keyframes |= self.get_volume_level(position + self.fade_in // 4, self.volume - FADE_DELTA)
+        keyframes |= self.get_volume_level(position, FADE_MIN)
+        keyframes |= self.get_volume_level(position + self.fade_in, self.volume)
 
+        if self.fade_out_position:
+            keyframes |= self.get_volume_level(self.frame_final_end, FADE_MIN)
+            position = self.frame_final_start + self.fade_out_position + self.fade_out
+        else:
+            position = self.frame_final_end
+        keyframes |= self.get_volume_level(position - self.fade_out // 4, self.volume - FADE_DELTA)
+        keyframes |= self.get_volume_level(position, FADE_MIN)
+        keyframes |= self.get_volume_level(position - self.fade_out, self.volume)
+
+        return keyframes
+
+    def get_mutes(self) -> type_keyframe:
+        keyframes: type_keyframe = {}
         position1 = None
         position2 = None
         for v in self.mutes:
@@ -96,18 +121,13 @@ class Strip():
                 position1 = v
                 continue
             position2 = v
-            fades |= self.get_volume_level(self.frame_final_start + position1, self.volume)
-            fades |= self.get_volume_level(self.frame_final_start + position1 + 2, FADE_MIN)
-            fades |= self.get_volume_level(self.frame_final_start + position2 - 2, FADE_MIN)
-            fades |= self.get_volume_level(self.frame_final_start + position2, self.volume)
+            keyframes |= self.get_volume_level(self.frame_final_start + position1, self.volume)
+            keyframes |= self.get_volume_level(self.frame_final_start + position1 + 2, FADE_MIN)
+            keyframes |= self.get_volume_level(self.frame_final_start + position2 - 2, FADE_MIN)
+            keyframes |= self.get_volume_level(self.frame_final_start + position2, self.volume)
             position1 = None
             position2 = None
-
-        fades |= self.get_volume_level(self.frame_final_end, FADE_MIN)
-        fades |= self.get_volume_level(self.frame_final_end - self.fade_out // 4, self.volume - 10)
-        fades |= self.get_volume_level(self.frame_final_end - self.fade_out, self.volume)
-
-        return '\n'.join(fades.values())
+        return keyframes
 
     def get_offset_duration_position(self, strip):
         try:
@@ -116,12 +136,36 @@ class Strip():
             self.offset, self.duration_ = strip['offset_duration_position']
             self.position = strip.get('position', -self.fade_in)
 
+    def get_volume_keyframes(self) -> type_keyframe:
+        keyframes: type_keyframe = {}
+        if 'mute_sound' in self.flags:
+            return keyframes
+        keyframes |= self.get_fades()
+        keyframes |= self.get_mutes()
+        keyframes |= self.get_volume_levels_by_keyframes()
+        return keyframes
+
     def get_volume_level(self, position, volume):
         delta = self.get_delta()
         position -= 1
         r = {}
         r[position] = self.xml_fade.format(position * delta, volume)
         return r
+
+    def get_volume_levels_by_keyframes(self) -> type_keyframe:
+        constants = dict(D=self.volume, FM=FADE_MIN)
+        keyframes: type_keyframe = {}
+        position = None
+        volume = None
+        for v in self.volume_levels:
+            if position is None:
+                position = v
+                continue
+            volume = constants.get(v, v)
+            keyframes |= self.get_volume_level(self.frame_final_start + position, volume)
+            position = None
+            volume = None
+        return keyframes
 
     def get_xml(self):
         delta = self.get_delta()
@@ -130,32 +174,57 @@ class Strip():
         left_frame_final_end = left.frame_final_end if left else 1
         xml = []
         if left_frame_final_end != self.frame_final_start:
-            duration = self.frame_final_start - left_frame_final_end
-            duration = self.extend_empty(left_fade_out, duration)
-            xml.append(self.xml_empty.format(duration * delta))
+            if (self.is_audio() and 'mute_sound' in self.flags) or (self.is_video() and 'mute_movie' in self.flags):
+                duration = self.frame_final_end - left_frame_final_end
+                duration = self.extend_empty(left_fade_out, duration)
+                xml.append(self.xml_empty.format(duration * delta))
+                return '\n'.join(xml)
+            else:
+                duration = self.frame_final_start - left_frame_final_end
+                duration = self.extend_empty(left_fade_out, duration)
+                xml.append(self.xml_empty.format(duration * delta))
         channel = self.get_audio_channel()
-        xml.append(self.xml.format(self.offset * delta, channel, self.duration * delta, self.filepath))
+        xml.append(self.xml.format(self.offset * delta, channel, self.duration * delta, self, self.filepath))
         return '\n'.join(xml)
+
+    def is_audio(self):
+        return True if self.type == 'AUDIO' else False
+
+    def is_video(self):
+        return not self.is_audio()
 
 
 channels: dict[int, Channel] = {}
 sequences: list[Strip] = []
 
 
-def get_last_sequence():
-    try:
-        return sequences[-1]
-    except IndexError:
-        return None
+def get_strips(strips: list[type_strip], parent: type[Strip] | Strip) -> None:
+    for i, strip in enumerate(strips):
+        if 'filepath' not in strip:
+            continue
 
+        strip['fades'] = strip.get('fades', [0, 0])
+        if len(strip['fades']) == 2:
+            strip['fades'] += [0, 0]
+        try:
+            strip['fades'][0] = strips[i - 1]['crossfade']
+        except (IndexError, KeyError):
+            pass
+        try:
+            strip['fades'][1] = strips[i + 1]['crossfade']
+        except (IndexError, KeyError):
+            pass
+        s = Strip(strip, parent=parent)
+        sequences.append(s)
 
-def get_left_sequence(strip, sequences=sequences):
-    left = None
-    for v in sequences:
-        if v == strip:
-            return left
-        left = v
-    return None
+        if s.channel not in channels:
+            channels[s.channel] = Channel(id=s.channel)
+        channels[s.channel].sequences.append(s)
+
+        if 'strips' in strip:
+            get_strips(strip['strips'], parent=s)
+
+        parent = s
 
 
 def load_yaml_config(file):
@@ -167,26 +236,8 @@ def load_yaml_config(file):
             volume_levels = config['volume_levels']
 
 
-def main():
-    for i, strip in enumerate(strips):
-        if 'filepath' not in strip:
-            continue
-
-        strip['fades'] = strip.get('fades', [0, 0])
-        try:
-            strip['fades'][0] = strips[i - 1]['crossfade']
-        except (IndexError, KeyError):
-            pass
-        try:
-            strip['fades'][1] = strips[i + 1]['crossfade']
-        except (IndexError, KeyError):
-            pass
-        s = Strip(strip)
-        sequences.append(s)
-
-        if s.channel not in channels:
-            channels[s.channel] = Channel(id=s.channel)
-        channels[s.channel].sequences.append(s)
+def main() -> None:
+    get_strips(strips, parent=Strip)
 
     id = None
     is_fade = False
@@ -214,7 +265,9 @@ def main():
                 is_fade = False
                 if id in channels:
                     for sequence in channels[id].sequences:
-                        print(sequence.get_fades_mutes())
+                        keyframes = sequence.get_volume_keyframes()
+                        if keyframes:
+                            print('\n'.join(keyframes.values()))
 
 
 if __name__ == '__main__':
