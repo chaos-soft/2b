f7469a6d4891419e0a797ccfa91393ca23aee2af
chaos-soft <mail@57st.su>
2025-12-06 21:55:03 +0300

    Удаление плагинов

--- cinelerra_generator.py
+++ cinelerra_generator.py
@@ -46,12 +46,6 @@ class Strip():
     )
     xml_empty: str = '<EDIT STARTSOURCE=0 CHANNEL=0 LENGTH={} HARD_LEFT=0 HARD_RIGHT=0 COLOR=0 GROUP_ID=0></EDIT>'
     xml_fade: str = '<AUTO POSITION={} VALUE={} VALUE1=0 CONTROL_IN_VALUE=0 CONTROL_OUT_VALUE=0 TANGENT_MODE=0></AUTO>'
-    xml_plugin_empty: str = (
-        '<PLUGIN LENGTH={} TYPE=0 TITLE="">\n'
-        '<IN></IN><OUT></OUT><ON></ON>\n'
-        '<KEYFRAME POSITION=0 DEFAULT=1></KEYFRAME>\n'
-        '</PLUGIN>'
-    )
 
     def __init__(self, strip, parent):
         self.append = strip.get('append', '')
@@ -64,7 +58,6 @@ class Strip():
         self.get_offset_duration_position(strip)
         self.mutes = strip.get('mutes', [])
         self.mutes_config = strip.get('mutes_config', [])
-        self.plugins = strip.get('plugins', [])
         self.volume_levels = strip.get('volume_levels', [])
         self.volume = strip.get('volume', 0)
         if not self.volume:
@@ -234,23 +227,6 @@ class Strip():
         else:
             return ''
 
-    def get_xml_plugins(self) -> str:
-        if not self.plugins:
-            return ''
-        else:
-            return '\n'.join(map(lambda v: v.format(self.duration), self.plugins))
-
-    def get_xml_plugins_empty(self) -> str:
-        if not self.plugins:
-            return ''
-        left = channels[self.channel].get_left_sequence(self) or Strip
-        if left.frame_final_end != self.frame_final_start:
-            duration = self.frame_final_start - left.frame_final_end
-            duration = self.extend_empty(duration, left.fade_out, left.fade_out_position, left.cut)
-            return self.xml_plugin_empty.format(duration)
-        else:
-            return ''
-
     def is_audio(self):
         return True if self.type == 'AUDIO' else False
 
@@ -330,15 +306,6 @@ def main() -> None:
                         print('\n'.join(keyframes.values()))
 
             print(line)
-
-            if line.startswith('</SPEEDAUTOS>') and type == 'VIDEO' and channel_id in channels:
-                for sequence in channels[channel_id].sequences:
-                    if sequence.plugins:
-                        print('<PLUGINSET RECORD=1>')
-                        for xml in [sequence.get_xml_plugins_empty(), sequence.get_xml_plugins().rstrip()]:
-                            if xml:
-                                print(xml)
-                        print('</PLUGINSET>')
 
 
 if __name__ == '__main__':
